bc7bfd9b7e40f3b5149a529b3b8779b6f3f143e1
Wail Abou <abou.w@hotmail.com>
2024-12-03 00:04:54 +0100

    Update node-thermal-printer.d.ts

--- node-thermal-printer.d.ts
+++ node-thermal-printer.d.ts
@@ -301,7 +301,7 @@ declare class ThermalPrinter {
     width?: number;
     cols?: number;
     bold?: boolean;
-  }[], newLine: boolean = true): void;
+  }[], newLine: boolean): void;
 
   /**
    * Check if printer is connected
